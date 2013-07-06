7146c925a52ce93aaea24aec23e21998d5cb06cc
Andreas Brekken <a@abrkn.com>
2013-07-06 17:26:53 +0200

    Fix console.log for IE.

--- web/util/debug.js
+++ web/util/debug.js
@@ -1,18 +1,13 @@
 var util = require('util')
-, noop = function() {}
 
 module.exports = function(name) {
-    if (typeof console == 'undefined') {
-        return noop
-    }
-
-    if (typeof console.log == 'undefined') {
-        return noop
-    }
-
     name = name || ''
 
     return function() {
+        if (typeof console === undefined || typeof console.log === undefined) {
+            return
+        }
+
         try {
             var message = util.format.apply(util, arguments)
             console.log('[' + name + '] ' + message)
